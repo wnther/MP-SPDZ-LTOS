fc188fe8df17a08fa477637622f683c935319f55
daniel <daniel@LegionDaniel>
2025-04-14 14:44:37 +0200

    small changes and investigation

--- Protocols/LTOS.hpp
+++ Protocols/LTOS.hpp
@@ -248,7 +248,10 @@ void SecureShuffle<T>::apply_multiple(StackedVector<T>& a, vector<size_t>& sizes
 template<class T>
 void SecureShuffle<T>::apply_multiple(StackedVector<T> &a, vector<size_t> &sizes, vector<size_t> &destinations,
     vector<size_t> &sources, vector<size_t> &unit_sizes, vector<shuffle_type> &shuffles, vector<bool> &reverse) {
-    (void) a;
+
+    //Debug print
+    clearprint_for_party(proc);
+    println_for_party(proc, a);
     
     const auto n_shuffles = sizes.size();
     assert(sources.size() == n_shuffles);
